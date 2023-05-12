8d48f8fbc145505529390f61bfb012b0be17dc4a
Jean <jean.leduc80@gmail.com>
2023-05-12 14:40:48 +0200

    cc

--- main.cpp
+++ main.cpp
@@ -12,34 +12,49 @@ int main(int argc, char **argv)
 {
 	Ball ball(200, 250, 10, 400);
 	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-	
-	sf::RectangleShape rectangle;
-	rectangle.setSize(sf::Vector2f(75, 25));
-	rectangle.setFillColor(sf::Color::Cyan);
-	rectangle.setOutlineColor(sf::Color::Blue);
-	rectangle.setOutlineThickness(2);
-	rectangle.setPosition(50, 50);
+
+	const int numRectanglesWidth = 8; // Nombre de rectangles en largeur
+	const int numRectanglesHeight = 5; // Nombre de rectangles en hauteur
+	sf::Vector2f rectangleSize(75, 25); // Taille des rectangles
+	float spacingX = 10.0f; // Espacement horizontal entre les rectangles
+	float spacingY = 10.0f; // Espacement vertical entre les rectangles
+
+	// Créer un tableau de rectangles
+	std::vector<sf::RectangleShape> rectangles(numRectanglesWidth * numRectanglesHeight);
+	for (int i = 0; i < numRectanglesHeight; ++i)
+	{
+		for (int j = 0; j < numRectanglesWidth; ++j)
+		{
+			sf::RectangleShape rectangle;
+			rectangle.setSize(rectangleSize);
+			rectangle.setFillColor(sf::Color::Cyan);
+			rectangle.setOutlineColor(sf::Color::Blue);
+			rectangle.setOutlineThickness(2);
+			float posX = 50 + (j * (rectangleSize.x + spacingX)); // Position en X
+			float posY = 50 + (i * (rectangleSize.y + spacingY)); // Position en Y
+			rectangle.setPosition(posX, posY);
+			rectangles[i * numRectanglesWidth + j] = rectangle;
+		}
+	}
 
 	sf::RectangleShape rdr2;
 	rdr2.setSize(sf::Vector2f(window.getSize().x, window.getSize().y));
-	
+
 	sf::Clock clock;
 	sf::Vector3f xFactor(10, 20, 30);
 	float ellapsedTime = 0;
 
-	// on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+	// On fait tourner le programme jusqu'à ce que la fenêtre soit fermée
 	while (window.isOpen())
 	{
 		ellapsedTime = clock.restart().asSeconds();
-		// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+		// On inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// évènement "fermeture demandée" : on ferme la fenêtre
+			// Évènement "fermeture demandée" : on ferme la fenêtre
 			if (event.type == sf::Event::Closed)
 				window.close();
-
-
 		}
 
 		ball.move(ellapsedTime);
@@ -47,8 +62,12 @@ int main(int argc, char **argv)
 
 		window.clear();
 
+		// Dessiner les rectangles dans la boucle principale
+		for (const auto& rectangle : rectangles)
+		{
+			window.draw(rectangle);
+		}
 
-		window.draw(rectangle);
 		ball.draw(window);
 		window.display();
 	}
